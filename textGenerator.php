7ea3b8543cfd4189756221328a96759a8e0d2e35
Igor <ig0r74@yandex.ru>
2016-07-12 16:27:19 +0300

    Create textGenerator.php

---
+++ textGenerator.php
@@ -0,0 +1,31 @@
+<?php
+/* Сначала проверяем на всякий случай, нужное ли нам событие произошло */
+if ($modx->event->name != "OnDocFormSave") {return;}
+
+if ($resource->get('description') != "") {return;}
+
+function textGenerator($text)
+   {
+   static $result;
+   if (preg_match("/^(.*)\{([^\{\}]+)\}(.*)$/isU", $text, $matches))
+      {
+      $p = explode('|', $matches[2]);
+      foreach ($p as $comb)
+         textGenerator($matches[1].$comb.$matches[3]);
+      }
+   else
+      {
+      $result[] = $text;
+      return 0;
+      }
+   return array_values(array_unique($result));
+   }
+
+$string = $modx->getChunk('textGenerator'); //получаем исходный текст для генерации из чанка textGenerator
+$variation = textGenerator($string); // все сгенерированные варианты
+$count = count($variation); // подсчет общего кол-ва
+$result = $variation[rand(0, $count - 1)]; // выбираем случайный вариант
+
+$resource->set('description', $result); // Устанавливаем новые значения в поле "Описание" [[*description]]
+$resource->save(); // и сохраняем объект
+return;
